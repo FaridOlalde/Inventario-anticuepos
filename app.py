6b6725ff20969915f7998682e9d3c3d237cc4da7
Farid Olalde <faridolald@gmail.com>
2025-10-25 21:12:19 -0600

    Change header color in antibody inventory page

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def cargar_datos_drive():
 
 # --- CONFIGURACIÓN DE LA PÁGINA ---
 st.set_page_config(page_title="Inventario de Anticuerpos", layout="wide")
-st.markdown("<h1 style='color:#2E5283;'>🧬 Inventario de Anticuerpos</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='color: #DC343B;'>🧬 Inventario de Anticuerpos</h1>", unsafe_allow_html=True)
     
 st.caption("Busca los anticuerpos por nombre.")
 
